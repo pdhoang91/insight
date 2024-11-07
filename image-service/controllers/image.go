1a4bf24e7bd7c8b3e3e3953319df0a7eecc5af60
hoang-pd <hoangpham@cubicasa.com>
2024-11-07 13:30:25 +0700

    add

--- image-service/controllers/image.go
+++ image-service/controllers/image.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"crypto/rand"
 	"encoding/hex"
-	"image"
 	"io"
 	"mime/multipart"
 	"net/http"
@@ -11,9 +10,7 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/chai2010/webp"
 	"github.com/gin-gonic/gin"
-	"github.com/nfnt/resize"
 )
 
 // Danh sách các MIME type được phép
@@ -51,6 +48,74 @@ func generatePrefix() (string, error) {
 	return hex.EncodeToString(b), nil
 }
 
+// // UploadImageV2 .. Image upload handler (Giảm kích thước và chuyển đổi sang WebP)
+// func UploadImageV2(c *gin.Context) {
+// 	const maxUploadSize = 10 << 20 // 10 MB
+// 	c.Request.Body = http.MaxBytesReader(c.Writer, c.Request.Body, maxUploadSize)
+// 	if err := c.Request.ParseMultipartForm(maxUploadSize); err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": "File quá lớn"})
+// 		return
+// 	}
+
+// 	file, err := c.FormFile("image")
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": "Không có ảnh được tải lên"})
+// 		return
+// 	}
+
+// 	if !isAllowedMimeType(file) {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": "Định dạng ảnh không được hỗ trợ"})
+// 		return
+// 	}
+
+// 	src, err := file.Open()
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Không thể mở tệp ảnh"})
+// 		return
+// 	}
+// 	defer src.Close()
+
+// 	img, _, err := image.Decode(src)
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": "Định dạng ảnh không hợp lệ"})
+// 		return
+// 	}
+
+// 	maxWidth, maxHeight := 800, 800
+// 	img = resize.Thumbnail(uint(maxWidth), uint(maxHeight), img, resize.Lanczos3)
+
+// 	prefix, err := generatePrefix()
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Không thể tạo tên tệp duy nhất"})
+// 		return
+// 	}
+
+// 	safeFilename := strings.ReplaceAll(file.Filename, " ", "_")
+// 	filename := strings.TrimSuffix(safeFilename, filepath.Ext(safeFilename)) + ".webp"
+// 	uploadDir := "images/uploads"
+
+// 	if err := os.MkdirAll(uploadDir, os.ModePerm); err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Không thể tạo thư mục uploads"})
+// 		return
+// 	}
+
+// 	filePath := filepath.Join(uploadDir, prefix+"_"+filename)
+// 	dst, err := os.Create(filePath)
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Không thể lưu ảnh WebP"})
+// 		return
+// 	}
+// 	defer dst.Close()
+
+// 	if err := webp.Encode(dst, img, &webp.Options{Lossless: true}); err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Không thể chuyển đổi ảnh sang WebP"})
+// 		return
+// 	}
+
+// 	imageURL := "https://" + c.Request.Host + "/images/uploads/" + prefix + "_" + filename
+// 	c.JSON(http.StatusOK, gin.H{"url": imageURL})
+// }
+
 // UploadImageV2 .. Image upload handler (Giảm kích thước và chuyển đổi sang WebP)
 func UploadImageV2(c *gin.Context) {
 	const maxUploadSize = 10 << 20 // 10 MB
@@ -71,21 +136,21 @@ func UploadImageV2(c *gin.Context) {
 		return
 	}
 
-	src, err := file.Open()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Không thể mở tệp ảnh"})
-		return
-	}
-	defer src.Close()
+	// src, err := file.Open()
+	// if err != nil {
+	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": "Không thể mở tệp ảnh"})
+	// 	return
+	// }
+	// defer src.Close()
 
-	img, _, err := image.Decode(src)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Định dạng ảnh không hợp lệ"})
-		return
-	}
+	// img, _, err := image.Decode(src)
+	// if err != nil {
+	// 	c.JSON(http.StatusBadRequest, gin.H{"error": "Định dạng ảnh không hợp lệ"})
+	// 	return
+	// }
 
-	maxWidth, maxHeight := 800, 800
-	img = resize.Thumbnail(uint(maxWidth), uint(maxHeight), img, resize.Lanczos3)
+	// maxWidth, maxHeight := 800, 800
+	// img = resize.Thumbnail(uint(maxWidth), uint(maxHeight), img, resize.Lanczos3)
 
 	prefix, err := generatePrefix()
 	if err != nil {
@@ -97,10 +162,10 @@ func UploadImageV2(c *gin.Context) {
 	filename := strings.TrimSuffix(safeFilename, filepath.Ext(safeFilename)) + ".webp"
 	uploadDir := "images/uploads"
 
-	if err := os.MkdirAll(uploadDir, os.ModePerm); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Không thể tạo thư mục uploads"})
-		return
-	}
+	// if err := os.MkdirAll(uploadDir, os.ModePerm); err != nil {
+	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": "Không thể tạo thư mục uploads"})
+	// 	return
+	// }
 
 	filePath := filepath.Join(uploadDir, prefix+"_"+filename)
 	dst, err := os.Create(filePath)
@@ -110,10 +175,10 @@ func UploadImageV2(c *gin.Context) {
 	}
 	defer dst.Close()
 
-	if err := webp.Encode(dst, img, &webp.Options{Lossless: true}); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Không thể chuyển đổi ảnh sang WebP"})
-		return
-	}
+	// if err := webp.Encode(dst, img, &webp.Options{Lossless: true}); err != nil {
+	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": "Không thể chuyển đổi ảnh sang WebP"})
+	// 	return
+	// }
 
 	imageURL := "https://" + c.Request.Host + "/images/uploads/" + prefix + "_" + filename
 	c.JSON(http.StatusOK, gin.H{"url": imageURL})
