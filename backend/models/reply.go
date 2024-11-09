60e6d10b04e53f51aff495d8064c1cee61c3a03f
hoang-pd <hoangpham@cubicasa.com>
2024-11-09 17:43:22 +0700

    handle delete

--- backend/models/reply.go
+++ backend/models/reply.go
@@ -11,9 +11,11 @@ import (
 type Reply struct {
 	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();" json:"id"` // ID của phản hồi
 	CommentID uuid.UUID `json:"comment_id"`                                      // Khóa ngoại từ bảng Comment
-	UserID    uuid.UUID `json:"user_id"`                                         // ID của tác giả
-	User      User      `gorm:"foreignKey:UserID" json:"user"`                   // Mối quan hệ với User
-	Content   string    `json:"content"`                                         // Nội dung phản hồi
-	CreatedAt time.Time `json:"created_at"`                                      // Thời gian tạo phản hồi
-	ClapCount uint64    `json:"clap_count" gorm:"-"`                             // Tổng số claps cho reply
+	//UserID    uuid.UUID `json:"user_id"`                                         // ID của tác giả
+	PostID    uuid.UUID `json:"post_id" gorm:"type:uuid;not null;index;constraint:OnDelete:CASCADE;"`
+	UserID    uuid.UUID `json:"user_id" gorm:"type:uuid;not null;constraint:OnDelete:CASCADE;"`
+	User      User      `gorm:"foreignKey:UserID" json:"user"` // Mối quan hệ với User
+	Content   string    `json:"content"`                       // Nội dung phản hồi
+	CreatedAt time.Time `json:"created_at"`                    // Thời gian tạo phản hồi
+	ClapCount uint64    `json:"clap_count" gorm:"-"`           // Tổng số claps cho reply
 }
--- backend/models/user_activity.go
+++ backend/models/user_activity.go
@@ -8,12 +8,29 @@ import (
 )
 
 // UserActivity lưu trữ thông tin về các hoạt động của người dùng.
+//type UserActivity struct {
+//	ID         uuid.UUID  `gorm:"type:uuid;default:uuid_generate_v4();" json:"id"` // ID của hoạt động
+//	UserID     uuid.UUID  `json:"user_id"`                                         // Khóa ngoại từ bảng User
+//	PostID     *uuid.UUID `json:"post_id,omitempty"`                               // Có thể null nếu là hành động cho Comment hoặc Reply
+//	CommentID  *uuid.UUID `json:"comment_id,omitempty"`                            // Có thể null nếu là hành động cho Post
+//	ReplyID    *uuid.UUID `json:"reply_id,omitempty"`                              // Có thể null nếu là hành động cho Post hoặc Comment
+//	ActionType string     `json:"action_type"`                                     // Loại hành động: clap, thả icon, v.v.
+//	CreatedAt  time.Time  `json:"created_at"`                                      // Thời gian tạo hoạt động
+//}
+
+// UserActivity lưu trữ thông tin về các hoạt động của người dùng.
 type UserActivity struct {
 	ID         uuid.UUID  `gorm:"type:uuid;default:uuid_generate_v4();" json:"id"` // ID của hoạt động
-	UserID     uuid.UUID  `json:"user_id"`                                         // Khóa ngoại từ bảng User
-	PostID     *uuid.UUID `json:"post_id,omitempty"`                               // Có thể null nếu là hành động cho Comment hoặc Reply
-	CommentID  *uuid.UUID `json:"comment_id,omitempty"`                            // Có thể null nếu là hành động cho Post
-	ReplyID    *uuid.UUID `json:"reply_id,omitempty"`                              // Có thể null nếu là hành động cho Post hoặc Comment
-	ActionType string     `json:"action_type"`                                     // Loại hành động: clap, thả icon, v.v.
-	CreatedAt  time.Time  `json:"created_at"`                                      // Thời gian tạo hoạt động
+	UserID     uuid.UUID  `json:"user_id" gorm:"type:uuid;not null;constraint:OnDelete:CASCADE;"`
+	PostID     *uuid.UUID `json:"post_id,omitempty" gorm:"type:uuid;constraint:OnDelete:CASCADE;"`
+	CommentID  *uuid.UUID `json:"comment_id,omitempty" gorm:"type:uuid;constraint:OnDelete:CASCADE;"`
+	ReplyID    *uuid.UUID `json:"reply_id,omitempty" gorm:"type:uuid;constraint:OnDelete:CASCADE;"`
+	ActionType string     `json:"action_type"` // Loại hành động: clap, thả icon, v.v.
+	CreatedAt  time.Time  `json:"created_at"`  // Thời gian tạo hoạt động
+
+	// Mối quan hệ với các mô hình khác
+	User    User    `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE;" json:"user"`
+	Post    Post    `gorm:"foreignKey:PostID;constraint:OnDelete:CASCADE;" json:"post,omitempty"`
+	Comment Comment `gorm:"foreignKey:CommentID;constraint:OnDelete:CASCADE;" json:"comment,omitempty"`
+	Reply   Reply   `gorm:"foreignKey:ReplyID;constraint:OnDelete:CASCADE;" json:"reply,omitempty"`
 }
