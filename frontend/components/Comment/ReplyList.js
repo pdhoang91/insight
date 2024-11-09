f6bd56661bba3744ebc40a8f257f92d1504c9930
hoang-pd <hoangpham@cubicasa.com>
2024-11-09 14:16:15 +0700

    change icon

--- frontend/components/Comment/ReplyList.js
+++ frontend/components/Comment/ReplyList.js
@@ -8,9 +8,9 @@ const ReplyList = ({ replies, commentId, mutate }) => {
     return <div className="text-red-500">Dữ liệu trả lời không hợp lệ.</div>;
   }
 
-  if (replies.length === 0) {
-    return <p className="text-gray-600">Chưa có trả lời nào. Hãy là người đầu tiên!</p>;
-  }
+  // if (replies.length === 0) {
+  //   return <p className="text-gray-600"></p>;
+  // }
 
   return (
     <ul className="space-y-4 mt-4 ml-8">
