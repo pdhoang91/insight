b86889e9c1aec1ee23d2c045eb69cdaa9c774c7f
hoang-pd <hoangpham@cubicasa.com>
2024-11-10 10:22:23 +0700

    fix upload image

--- frontend/components/Utils/ViewMoreButton.js
+++ frontend/components/Utils/ViewMoreButton.js
@@ -1,20 +1,4 @@
 // // components/Utils/ViewMoreButton.js
-// import React from 'react';
-
-// const ViewMoreButton = ({ onClick, className = '', children = 'see more' }) => {
-//   return (
-//     <button
-//       onClick={onClick}
-//       className={`mt-4 inline-block px-3 py-2 border border-gray-300 text-gray-600 rounded-full text-center transition-colors hover:bg-gray-100 ${className}`}
-//     >
-//       {children}
-//     </button>
-//   );
-// };
-
-// export default ViewMoreButton;
-
-
 //components/Utils/ViewMoreButton.js
 import React from 'react';
 
@@ -31,19 +15,3 @@ export const ViewMoreButton = ({ onClick, className = '', children = 'see more' 
 
 export default ViewMoreButton;
 
-// components/Utils/ViewMoreButton.js
-// import React from 'react';
-
-// const ViewMoreButton = ({ onClick, className = '', children = 'see more' }) => {
-//   return (
-//     <button
-//       onClick={onClick}
-//       className={`mt-4 text-green-600 text-sm hover:text-green-700 transition-colors ${className}`}
-//       style={{ background: 'none', border: 'none', padding: 0 }}
-//     >
-//       {children}
-//     </button>
-//   );
-// };
-
-// export default ViewMoreButton;
