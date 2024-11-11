82015a769d94a7826ed7b10d50760bed18dafd3c
hoang-pd <hoangpham@cubicasa.com>
2024-11-11 10:02:29 +0700

    add

--- frontend/services/searchService.js
+++ frontend/services/searchService.js
@@ -8,19 +8,30 @@ export const fetchStories = async (query, page = 1, limit = 10) => {
     });
 
     const data = response.data;
+    console.log("data", data);
+
+    // Kiểm tra định dạng của `data` và `data.data`
     if (!data || !Array.isArray(data.data) || typeof data.total_count !== 'number') {
-      throw new Error('Invalid response format for fetchStories');
+      return {
+        posts: [],
+        totalCount: 0,
+      };
     }
 
     return {
-      posts: data ? data.data:[],
+      posts: data.data,
       totalCount: data.total_count,
     };
   } catch (error) {
     console.error('Error fetching stories:', error);
-    throw error;
+    return {
+      posts: [],
+      totalCount: 0,
+    };
   }
 };
+
+
 export const fetchPeople = async (query, page = 1, limit = 10) => {
   try {
     const response = await axiosPublicInstance.get(`/search/people`, {
