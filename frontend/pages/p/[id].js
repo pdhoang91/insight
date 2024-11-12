298f70c26818229f0b48fa572d0333d1ea3cc0db
hoang-pd <hoangpham@cubicasa.com>
2024-11-12 22:08:09 +0700

    handle responesive

--- frontend/pages/p/[id].js
+++ frontend/pages/p/[id].js
@@ -1,31 +1,69 @@
+// // pages/p/[id].js
+// import { useRouter } from 'next/router';
+// import React from 'react';
+// import { usePost, usePostName } from '../../hooks/usePost';
+// import PostDetail from '../../components/Post/PostDetail';
+
+// const PostPage = () => {
+//   const router = useRouter();
+//   const { id } = router.query;
+
+//   //const { post, isLoading, isError, mutate } = usePost(id);
+//   const { post, isLoading, isError, mutate } = usePostName(id);
+
+//   if (isLoading) return <div>Loading...</div>;
+//   if (isError) return <div>Failed to load post.</div>;
+
+//   return (
+//     <div className="flex min-h-screen">
+//       {/* Khoảng trống bên trái */}
+//       <div className="hidden lg:block lg:w-2/12"></div>
+
+//       {/* Nội dung chính */}
+//       <main className="flex-1 p-6 overflow-auto">
+//         <PostDetail post={post} />
+//       </main>
+
+//       {/* Khoảng trống bên phải */}
+//       <div className="hidden lg:block lg:w-2/12"></div>
+//     </div>
+//   );
+// };
+
+// export default PostPage;
+
+
 // pages/p/[id].js
 import { useRouter } from 'next/router';
 import React from 'react';
-import { usePost, usePostName } from '../../hooks/usePost';
+import { usePostName } from '../../hooks/usePost';
 import PostDetail from '../../components/Post/PostDetail';
 
 const PostPage = () => {
   const router = useRouter();
   const { id } = router.query;
 
-  //const { post, isLoading, isError, mutate } = usePost(id);
   const { post, isLoading, isError, mutate } = usePostName(id);
 
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>Failed to load post.</div>;
+  if (isLoading) return <div className="flex justify-center items-center h-screen">Loading...</div>;
+  if (isError) return <div className="flex justify-center items-center h-screen">Failed to load post.</div>;
 
   return (
-    <div className="flex min-h-screen">
-      {/* Khoảng trống bên trái */}
-      <div className="hidden lg:block lg:w-2/12"></div>
+    <div className="flex flex-col lg:flex-row min-h-screen">
+      {/* Sidebar bên trái */}
+      <aside className="hidden lg:block lg:w-2/12 p-4">
+        {/* Nội dung sidebar bên trái */}
+      </aside>
 
       {/* Nội dung chính */}
-      <main className="flex-1 p-6 overflow-auto">
+      <main className="flex-1 p-4">
         <PostDetail post={post} />
       </main>
 
-      {/* Khoảng trống bên phải */}
-      <div className="hidden lg:block lg:w-2/12"></div>
+      {/* Sidebar bên phải */}
+      <aside className="hidden lg:block lg:w-2/12 p-4">
+        {/* Nội dung sidebar bên phải */}
+      </aside>
     </div>
   );
 };
