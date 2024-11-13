c40b93cf60c083220fba51186c2189c441082ca7
hoang-pd <hoangpham@cubicasa.com>
2024-11-13 12:12:45 +0700

    add

---
+++ frontend/pages/_document.js
@@ -0,0 +1,17 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* Liên kết đến favicon */}
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
