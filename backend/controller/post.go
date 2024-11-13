b3326ff1eb0fde006fba1c0434b926fbe578a8e1
hoang-pd <hoangpham@cubicasa.com>
2024-11-13 12:49:22 +0700

    handle slut

--- backend/controller/post.go
+++ backend/controller/post.go
@@ -149,6 +149,55 @@ func capitalizeFirstLetter(s string) string {
 	return strings.ToUpper(string(s[0])) + s[1:]
 }
 
+func removeDiacritics(input string) string {
+	var output []rune
+	for _, r := range input {
+		switch r {
+		case 'à', 'á', 'ả', 'ã', 'ạ', 'â', 'ầ', 'ấ', 'ẩ', 'ẫ', 'ậ', 'ă', 'ằ', 'ắ', 'ẳ', 'ẵ', 'ặ':
+			r = 'a'
+		case 'è', 'é', 'ẻ', 'ẽ', 'ẹ', 'ê', 'ề', 'ế', 'ể', 'ễ', 'ệ':
+			r = 'e'
+		case 'ì', 'í', 'ỉ', 'ĩ', 'ị':
+			r = 'i'
+		case 'ò', 'ó', 'ỏ', 'õ', 'ọ', 'ô', 'ồ', 'ố', 'ổ', 'ỗ', 'ộ', 'ơ', 'ờ', 'ớ', 'ở', 'ỡ', 'ợ':
+			r = 'o'
+		case 'ù', 'ú', 'ủ', 'ũ', 'ụ', 'ư', 'ừ', 'ứ', 'ử', 'ữ', 'ự':
+			r = 'u'
+		case 'ỳ', 'ý', 'ỷ', 'ỹ', 'ỵ':
+			r = 'y'
+		case 'đ':
+			r = 'd'
+		case 'À', 'Á', 'Ả', 'Ã', 'Ạ', 'Â', 'Ầ', 'Ấ', 'Ẩ', 'Ẫ', 'Ậ', 'Ă', 'Ằ', 'Ắ', 'Ẳ', 'Ẵ', 'Ặ':
+			r = 'A'
+		case 'È', 'É', 'Ẻ', 'Ẽ', 'Ẹ', 'Ê', 'Ề', 'Ế', 'Ể', 'Ễ', 'Ệ':
+			r = 'E'
+		case 'Ì', 'Í', 'Ỉ', 'Ĩ', 'Ị':
+			r = 'I'
+		case 'Ò', 'Ó', 'Ỏ', 'Õ', 'Ọ', 'Ô', 'Ồ', 'Ố', 'Ổ', 'Ỗ', 'Ộ', 'Ơ', 'Ờ', 'Ớ', 'Ở', 'Ỡ', 'Ợ':
+			r = 'O'
+		case 'Ù', 'Ú', 'Ủ', 'Ũ', 'Ụ', 'Ư', 'Ừ', 'Ứ', 'Ử', 'Ữ', 'Ự':
+			r = 'U'
+		case 'Ỳ', 'Ý', 'Ỷ', 'Ỹ', 'Ỵ':
+			r = 'Y'
+		case 'Đ':
+			r = 'D'
+		}
+		output = append(output, r)
+	}
+	return string(output)
+}
+
+func createSlug(title string) string {
+	// Loại bỏ dấu
+	title = removeDiacritics(title)
+	// Đưa về chữ thường và thay thế khoảng trắng bằng dấu gạch ngang
+	slug := strings.ToLower(title)
+	slug = strings.ReplaceAll(slug, " ", "-")
+	// Loại bỏ các ký tự đặc biệt còn lại
+	slug = regexp.MustCompile(`[^a-z0-9-]+`).ReplaceAllString(slug, "")
+	return slug
+}
+
 func CreatePost(c *gin.Context) {
 	var input struct {
 		Title      string   `json:"title" binding:"required"`
@@ -195,9 +244,10 @@ func CreatePost(c *gin.Context) {
 	}
 
 	// Create slug from Title
-	slug := strings.ToLower(input.Title)
-	slug = strings.ReplaceAll(slug, " ", "-")
-	slug = regexp.MustCompile(`[^a-zA-Z0-9-]+`).ReplaceAllString(slug, "")
+	// slug := strings.ToLower(input.Title)
+	// slug = strings.ReplaceAll(slug, " ", "-")
+	// slug = regexp.MustCompile(`[^a-zA-Z0-9-]+`).ReplaceAllString(slug, "")
+	slug := createSlug(input.Title)
 
 	// Ensure unique title_name
 	existingPost := models.Post{}
