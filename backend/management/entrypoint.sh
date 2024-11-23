c7749e90999229d455133b20a532c85a417d040f
hoang-pd <hoangpham@cubicasa.com>
2024-11-23 17:24:39 +0700

    refactor

--- backend/management/entrypoint.sh
+++ backend/management/entrypoint.sh
@@ -4,8 +4,5 @@ set -e
 # Đợi PostgreSQL sẵn sàng
 /wait-for-it.sh db:5432 -- echo "PostgreSQL is up"
 
-# Đợi Elasticsearch sẵn sàng
-/wait-for-it.sh elasticsearch:9200 -- echo "Elasticsearch is up"
-
 # Khởi động ứng dụng
-exec ./main
+exec ./management
