5375071a850e30a49826c0f4599a6e4e0a3fe79f
hoang-pd <hoangpham@cubicasa.com>
2025-08-10 06:24:57 +0700

    add

--- frontend/backup-unused-files/hooks/useUser.js
+++
@@ -1,9 +0,0 @@
-// hooks/useUser.js
-import { useContext } from 'react';
-import UserContext from '../context/UserContext';
-
-export const useUser = () => {
-  return useContext(UserContext);
-};
-
-export { useUser };
