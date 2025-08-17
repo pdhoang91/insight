a7b83dc50b91109f4783e8dd020997a16fb6c742
hoang-pd <hoangpham@cubicasa.com>
2025-08-18 06:19:57 +0700

    add

--- setup-ssl.sh
+++ setup-ssl.sh
@@ -58,7 +58,7 @@ echo "🔐 Generating SSL certificates..."
 docker-compose -f docker-compose.prod.yml run --rm certbot certonly \
     --webroot \
     --webroot-path=/var/www/certbot \
-    --email your-email@example.com \
+    --email pdhoang91@gmail.com \
     --agree-tos \
     --no-eff-email \
     -d www.insight.io.vn \
